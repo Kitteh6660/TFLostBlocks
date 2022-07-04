7b393c7b25f648dc32dfac6942b48f23aff018d8
Kitteh6660 <Kitteh6660@users.noreply.github.com>
2022-07-03 21:51:44 -0400

    Initial codebase
    Initial codebase of the addon Twilight Forest: The Lost Blocks

---
+++ src/main/java/kittehmod/tflostblocks/TFLostBlocksMod.java
@@ -0,0 +1,50 @@
+package kittehmod.tflostblocks;
+
+import kittehmod.tflostblocks.blockentities.ModBlockEntities;
+import kittehmod.tflostblocks.blocks.ModBlocks;
+import kittehmod.tflostblocks.blocks.ModWoodType;
+import kittehmod.tflostblocks.client.ClientHandler;
+import kittehmod.tflostblocks.items.ModItems;
+import net.minecraft.client.renderer.Sheets;
+import net.minecraft.world.level.block.state.properties.WoodType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TFLostBlocksMod.MOD_ID)
+public class TFLostBlocksMod
+{
+    public static final String MOD_ID = "tflostblocks";
+
+    public TFLostBlocksMod()
+    {
+    	ModBlocks.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    	ModItems.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    	ModBlockEntities.BLOCK_ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
+    	
+    	FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupCommon);
+    	if (FMLEnvironment.dist == Dist.CLIENT) {
+    		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
+    	}
+    }
+
+    private void setupCommon(final FMLCommonSetupEvent event)
+    {
+    	MinecraftForge.EVENT_BUS.register(new CommonEvents());
+        event.enqueueWork(() -> WoodType.register(ModWoodType.TOWERWOOD));
+    }
+    
+    private void setupClient(final FMLClientSetupEvent event)
+    {
+    	ClientHandler.setupRenderers();
+    	event.enqueueWork(() -> { Sheets.addWoodType(ModWoodType.TOWERWOOD);} );	
+    	
+    	MinecraftForge.EVENT_BUS.register(ClientHandler.class);
+    }
+
+}
---
+++ src/main/java/kittehmod/tflostblocks/blocks/MazestoneStairBlock.java
@@ -0,0 +1,31 @@
+package kittehmod.tflostblocks.blocks;
+
+import java.util.function.Supplier;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.StairBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import twilightforest.item.MazebreakerPickItem;
+
+public class MazestoneStairBlock extends StairBlock
+{
+
+	public MazestoneStairBlock(Supplier<BlockState> state, Properties properties) {
+		super(state, properties);
+	}
+
+	@Override
+	public void playerWillDestroy(Level world, BlockPos pos, BlockState state, Player player) {
+		super.playerWillDestroy(world, pos, state, player);
+		ItemStack stack = player.getItemInHand(InteractionHand.MAIN_HAND);
+
+		// damage the player's pickaxe
+		if (!world.isClientSide && !stack.isEmpty() && stack.getItem().canBeDepleted() && !(stack.getItem() instanceof MazebreakerPickItem)) {
+			stack.hurtAndBreak(16, player, (user) -> user.broadcastBreakEvent(InteractionHand.MAIN_HAND));
+		}
+	}
+}
